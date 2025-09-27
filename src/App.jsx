42b45474c137fd87b3d32fcc0c560ff05c5f94ac
Daniel Todd <dgtutv@gmail.com>
2025-09-26 22:03:09 -0700

    gifs load

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [gifs, setGifs] = useState([]);
+  const API_KEY = import.meta.env.VITE_API_KEY;
+  const limit = 20;
+  const URL = `https://api.giphy.com/v1/gifs/trending?api_key=${API_KEY}&limit=${limit}`;
+
+  useEffect(() => {
+    async function getGifs(){
+      try{
+        const response = await fetch(URL);
+        if(!response.ok){
+            throw new Error("Incorrect URL or connection issue.");
+        }
+        const data = await response.json();
+        return data.data;
+      } 
+      catch(error){
+          console.error("Error:", error);
+          return [];
+      }
+    }
+
+    getGifs().then((gifs) => setGifs(gifs));
+    
+  }, [])
+
+  const gridStyle = {
+    display: "flex",
+    flexWrap: "wrap",
+    gap: "10px"
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div style={gridStyle}>
+        {gifs.map((gif) => (
+          <img key={gif.id} src={gif.images.fixed_height.url} alt={gif.title} />
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
